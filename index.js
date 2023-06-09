a1bfaea298012cfe2fc024fc00ad754d72e735cc
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-06-09 16:10:44 +0530

    created drag and drop

---
+++ index.js
@@ -0,0 +1,33 @@
+// Get the containers
+const dragContainer = document.querySelector('.drag-container');
+const dropContainer = document.querySelector('.drop-container');
+
+const items = document.querySelectorAll('.item');
+for(const item of items) {
+  item.addEventListener("dragstart", function(e){
+    let selected = e.target;
+
+    dropContainer.addEventListener("dragover", function(e){
+      e.preventDefault();
+    })
+
+    dropContainer.addEventListener("drop", function(e){
+      dropContainer.appendChild(selected);
+      selected = null;
+    })
+  })
+}
+
+function displaySuccessMessage() {
+  const successMessage = document.createElement('p');
+  successMessage.classList.add('success-message');
+  successMessage.textContent = 'Item dropped successfully!';
+  dropContainer.appendChild(successMessage);
+}
+
+function resetContainers() {
+  items.forEach(item => {
+    item.style.display = 'flex';
+    dragContainer.appendChild(item);
+  });
+}
